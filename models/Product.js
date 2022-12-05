485324821ccc30bbc681b4f9a3403914d73efe3d
Guido Seia <guidoseia5@gmail.com>
2022-12-05 19:11:23 -0300

    nuevo modelo de usuarios y productos

---
+++ models/Product.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema ({
+    brand: {type: String, required: true},
+    model: {type: String, required: true},
+    description: {type: String, required: true, min: 5, max: 300},
+    price: {type: Number, required: true, min: 1},
+    photo: {type: Array, required: true},
+})
+
+const Product = mongoose.model(
+    "products",
+    ProductSchema
+)
+
+module.exports = Product
