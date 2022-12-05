ee70174c8c49898131ae4f7097e7be7f14b2d5ff
Guido Seia <guidoseia5@gmail.com>
2022-12-05 19:10:50 -0300

    importacion ded rutas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const productsRouter = require('./products')
+const usersRouter = require('./usersLogin')
+
 router.get('/', function(req, res, next) {
   res.json();
+
+  router.use('/auth', usersRouter)
+  router.use('/products',productsRouter)
+
 });
 
 module.exports = router;
